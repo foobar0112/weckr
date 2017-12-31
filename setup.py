6ea9a5ad139ecd9151d29ca23573ac83fca40454
Felix Buehler <account@buehler.rocks>
2017-12-31 19:37:13 +0100

    removing click-dep by using argparse and logging & reducing volume of music while news are playing
    fixing: #3 #9 #11 #12 #15

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name='weckr',
-    version='0.1',
+    version='0.2',
     py_modules=['weckr'],
     install_requires=[
-        'Click',
         'python-vlc',
         'python-dateutil'
     ],
-    entry_points='''
-        [console_scripts]
-        weckr=weckr:weckr
-    ''',
+
+    entry_points = {
+        'console_scripts': ['weckr=weckr:main'],
+    }
 )
--- weckr.py
+++ weckr.py
@@ -1,72 +1,23 @@
 import os
-import click as c
-import time as t
+import sys
+import argparse
+import logging
+import time
 import vlc
+import datetime
+
 import xml.etree.ElementTree as Et
 import urllib.request
-import datetime
 from dateutil.parser import parse
 
-v = False
-# TODO: complete list of supported extensions
 formats = ['mp3', 'wav', 'opus', 'wma', 'ogg']
 
 
-def validate_time(ctx, param, val: str) -> (int, int):
-    """
-    callback for validation of inserted time. time should be in format h:m
-    :param ctx:
-    :param param:
-    :param val: value
-    :return: tuple (h,m) of hour and minute
-    """
-    try:
-        h, m = map(int, val.split(':', 2))
-        if 0 < h > 23 or 0 < m > 59:
-            raise c.BadParameter('This is not a valid time.')
-        return h, m
-    except ValueError:
-        raise c.BadParameter('h:m format for time parameter --time is required.')
-
-
-def validate_news_time(ctx, param, val: int) -> int:
-    """
-    callback for validation of news delay time. should be positive integer
-    :param ctx:
-    :param param:
-    :param val: value
-    :return: news delay time
-    """
-    if val is int and val < 0:
-        raise c.BadParameter('This is not a valid news delay time.')
-    return val
-
-
-@c.command()
-@c.argument('sound_file',
-            type=c.Path(exists=True, resolve_path=True))
-@c.option('-t', '--time',
-          prompt='Alarm time',
-          help='Takes alarm time (h:m).',
-          callback=validate_time)
-@c.option('-n', '--news',
-          help='Includes latest DLF news (default: 5 min delay).',
-          is_flag=True)
-@c.option('-N', '--news-time',
-          help='Includes latest DLF news with specific time delay (in min).',
-          type=int,
-          callback=validate_news_time)
-@c.option('-v', '--verbose',
-          help='Toggles verbose mode.',
-          is_flag=True)
-def weckr(sound_file: str, time: (int, int), news: bool, news_time: int, verbose: bool) -> None:
+def weckr(sound_file: str, wakeup_time: datetime.time, news: bool, news_time: int, music_fade: int,
+          music_max: int) -> None:
     """
     command line alarm clock takes audio file or directory of audio files
     """
-
-    # handle options
-    global v
-    v = verbose
 
     if news_time is not None:
         news = True
@@ -75,104 +26,97 @@ def weckr(sound_file: str, time: (int, int), news: bool, news_time: int, verbose
 
     news_echo = ''
     if news:
-        news_echo = ' and the ' + c.style('news', fg='white')
-        if v:
-            news_echo += ' after ' + str(news_time) + ' min'
+        news_echo = ' and the ' + 'news'
+        # if v:
+        news_echo += ' after ' + str(news_time) + ' min'
 
-    # check path
-    ext = ''
-    if os.path.isfile(sound_file):
-        ext = get_ext(sound_file)
-    else:
-        for (root, dirs, files) in os.walk(sound_file):
-            count = len([f for f in files if os.path.splitext(f)[1][1:] in formats])
-            if count == 0:
-                c.echo(c.style('\nError:', bold=True, fg='red') +
-                       ' The directory ' + c.style(sound_file, fg='blue') +
-                       ' doesn\'t contain any audio files.')
-                exit(1)
-            else:
-                num = datetime.now().day % count
-                verb('Selected file ' + str(num) + ' in directory: ' + c.style(sound_file, fg='blue'))
-                sound_file = os.sep.join([root, files[num]])
-                ext = get_ext(sound_file)
-                break
+    # TODO check if sound_file is playable
 
     # compute sleep
-    h, m = time
-
     now = datetime.datetime.now()
-    time = datetime.time(h, m, 0)
-    alarm_time = datetime.datetime.combine(now.date(), time)
+    alarm_time = datetime.datetime.combine(now.date(), wakeup_time)
     if alarm_time < now:
         alarm_time += datetime.timedelta(days=1)
     delta = alarm_time - now
 
     # announce sleep
-    verb('\nI\'ll wake you up at ' + c.style(str(h) + ':' + str(m).zfill(2), bold=True, fg='white') +
-         ' playing ' + c.style(os.path.basename(sound_file), fg='blue') + news_echo + ' in less then ' +
-         c.style(str(delta.seconds//3600) + ' h ' + str((delta.seconds//60)%60) + ' min', bold=True, fg='white'))
+    log.debug('I\'ll wake you up at ' + str(alarm_time.hour) + ':' + str(alarm_time.minute).zfill(2) +
+              ' playing ' + os.path.basename(sound_file) + news_echo + ' in less then ' +
+              str(delta.seconds // 3600) + ' h ' + str((delta.seconds // 60) % 60 + 1) + ' min')
 
-    verb('\nNow I\'m going to sleep. You also should do so.')
+    log.info('Now I\'m going to sleep. You also should do so.')
 
     # finally sleep
-    t.sleep(delta.total_seconds())
+    time.sleep(delta.total_seconds())
 
     # wake up
-    verb('\n*yawning*')
-    c.echo(c.style('\n\nWake up!!\n', bold=True, bg='red', fg='white', blink=True))
+    log.info('*yawning*')
+    log.debug('Wake up!!')
 
     # play background sound
-    play_sound(sound_file, ext, 60)
+    play_sound(sound_file, music_fade, music_max)
 
     # play news if wanted
     if news:
-        verb('\nGoing to play news in ' + c.style(str(news_time), fg='white') + ' min.')
-        t.sleep(news_time * 60)
-        play_news()
+        log.info('Going to play news in ' + str(news_time) + ' min.')
+        time.sleep(news_time * 60 - 5)
+        play_news(5, 40, music_max)
 
     input("\nPress ENTER to stop")
 
 
-def play_sound(sound_path: str, ext: str, fade: int) -> None:
+def play_sound(sound_path: str, music_fade: int, music_max: int) -> None:
     """
     triggers sounds
     :param sound_path: path to main sound file
-    :param ext: file extension of sound file
-    :param fade: time in seconds to fade in sound
+    :param music_fade: time in seconds to fade in sound
+    :param music_max: maximum voulme level
     """
-    if ext != '':
-        verb('\nPlaying file: ' + c.style(os.path.basename(sound_path), fg='blue'))
+    log.debug('Playing file: ' + os.path.basename(sound_path))
 
-        s = vlc.MediaPlayer(sound_path)
-        s.play()
+    global vlc_music
+    vlc_music = vlc.MediaPlayer(sound_path)
+    vlc_music.play()
 
-        for vol in range(0, 100):
-            s.audio_set_volume(vol)
-            t.sleep(fade / 100)
-            verb('Increasing volume to: ' + str(vol) + '%')
+    for vol in range(0, music_max):
+        vlc_music.audio_set_volume(vol)
+        time.sleep(music_fade / 100)
+        log.info('Increasing volume to: ' + str(vol) + '%')
 
 
-def play_news() -> None:
+def play_news(news_fade: int, music_volume: int, music_max: int) -> None:
     """
     fetches DLF news XML and play latest (of full hour) news
     """
     resp = urllib.request.urlopen('http://www.deutschlandfunk.de/podcast-nachrichten.1257.de.podcast.xml')
 
-    verb('\nFetched news XML successfully.')
+    log.debug('\nFetched news XML successfully.')
 
     xml = resp.read().decode('utf-8')
     root = Et.fromstring(xml).find('channel')
     for item in root.findall('item'):
         if parse(item.find('pubDate').text).time().minute == 0:
             news_url = item.find('link').text
-            n = vlc.MediaPlayer(news_url)
-            n.audio_set_volume(100)
-            n.play()
+            for vol in range(music_max, music_volume, -1):
+                if vlc_music:
+                    vlc_music.audio_set_volume(vol)
+                    time.sleep(news_fade / 100)
+                    log.info('Decrease volume to: ' + str(vol) + '%')
+            vlc_news = vlc.MediaPlayer(news_url)
+            log.debug("now playing news")
+            vlc_news.audio_set_volume(100)
+            vlc_news.play()
+            while vlc_news.get_state() != vlc.State.Ended:
+                time.sleep(100)
+            for vol in range(music_volume, music_max):
+                if vlc_music:
+                    vlc_music.audio_set_volume(vol)
+                    time.sleep(news_fade / 100)
+                    log.info('Increasing volume to: ' + str(vol) + '%')
             break
 
 
-def get_ext(sound: str) -> str:
+def get_ext(sound: str) -> None:
     """
     gets files extension if valid
     :param sound:
@@ -180,17 +124,63 @@ def get_ext(sound: str) -> str:
     """
     ext = os.path.splitext(sound)[1][1:]
     if ext not in formats:
-        c.echo(c.style('\nError:', bold=True, fg='red') +
-               ' The audio file extension ' + c.style(ext, fg='white') +
-               ' is not supported. Please use one of these: ' + ', '.join(formats) + '.')
+        log.warning('Error:' +
+                    ' The audio file extension ' + ext +
+                    ' is not supported. Please use one of these: ' + ', '.join(formats) + '.')
         exit(1)
-    return ext
 
 
-def verb(msg: str) -> None:
-    """
-    helper function for printing verbose messages
-    :param msg: message
-    """
-    if v:
-        c.echo(msg)
+def main():
+    global log
+    log = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser(description="weckr: a CLI alarm clock with unicorn features ")
+    parser.add_argument("-v", "--verbose", dest="verbose_count",
+                        action="count", default=0,
+                        help="increases log verbosity for each occurence.")
+    parser.add_argument("-t",
+                        "--time",
+                        help="Alarm time",
+                        required=True,
+                        type=lambda d: datetime.datetime.strptime(d, '%H:%M').time())
+    parser.add_argument("-n", "--news", help="Includes latest DLF news (default: 5 min delay).",
+                        action="store_true")
+    parser.add_argument("-N",
+                        "--news-time",
+                        help="Includes latest DLF news with specific time delay (in min).",
+                        type=lambda n: int(n) if int(n) > 0 else 0)
+    parser.add_argument("-m",
+                        "--max-volume",
+                        help="set the maximum volume of music",
+                        type=lambda n: int(n) if int(n) > 0 and int(n) < 100 else 50)
+    parser.add_argument("-f",
+                        "--fade-time",
+                        help="time the music is fading",
+                        type=lambda n: int(n) if int(n) > 0 else 30)
+    parser.add_argument('sound_file',
+                        type=str)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    # handler = logging.FileHandler('weckr.log')
+    # handler.setLevel(logging.INFO)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # handler.setFormatter(formatter)
+    # log.addHandler(handler)
+
+    output = logging.StreamHandler()
+    output_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    output.setFormatter(output_formatter)
+    log.addHandler(output)
+
+    log.setLevel(max(3 - args.verbose_count, 0) * 10)
+
+    weckr(args.sound_file, args.time, args.news, args.news_time, args.fade_time, args.max_volume)
+
+
+if __name__ == "__main__":
+    main()
